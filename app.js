1410e28997cf8810cc5dce673c4aa3fcdb2becb2
Skyrion9 <ottomanobi@gmail.com>
2020-11-20 17:46:23 +0300

    favicon

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -12,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server','views'));
 app.set('view engine', 'pug');
+app.use(favicon(path.join(__dirname, 'public', 'images','favicon.ico')));
 
 app.use(logger('dev'));
 app.use(express.json());
