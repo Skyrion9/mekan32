79b3028116d3f30b16e2dec156404849a680e3bc
Skyrion9 <ottomanobi@gmail.com>
2020-11-30 21:18:13 +0300

    responsive

--- app_server/controllers/main.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-//anasayfa yoneten kontrolcu metod 
-module.exports.index=function(req, res, next) {
-  res.render('index', { title: 'Mert Kuş'});
-  
-}
